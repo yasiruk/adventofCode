fdab27e8e7e4e4f4ebfdae6b0d61de8737648de3
Yasiru Kassapa <wik2kassa@gmail.com>
2023-12-10 23:12:12 +0100

    Day 4 part 1 & 2

--- 2023/day4/part1n2.c
+++ 2023/day4/part1n2.c
@@ -5,8 +5,11 @@ int main(int argc, char **argv) {
   FILE *in = fopen(argv[1], "r");
   int c;
   int total = 0;
-  int win_length = 10;
-  int ticket_length = 25;
+  int win_length = 10;    // input :10, example 5
+  int ticket_length = 25; // input: 25, example 8
+  int scores[193] = {0};
+  int copies[193] = {0};
+
   while (fscanf(in, " Card %d:", &c) == 1) {
     for (int i = 0; i < 100; i++) {
       arr[i] = 0;
@@ -20,22 +23,34 @@ int main(int argc, char **argv) {
       arr[n] = 1;
     }
     fscanf(in, " | ");
+    printf("|");
     for (int i = 0; i < ticket_length; i++) {
       fscanf(in, "%d", &n);
       printf("%d", n);
       if (arr[n] == 1) {
         printf("*");
-        if (score == 0) {
-          score = 1;
-        } else {
-          score *= 2;
-        }
+        score++;
       }
       printf(" ");
     }
     printf(" -> %d %d\n", c, score);
+    scores[c] = score;
     total += score;
   }
-  printf("%d\n", total);
+
+  int total_copies = 0;
+  for (int i = 1; i <= c; i++) {
+    copies[i] += 1;
+    printf("Card %d (%d) %d\n", i, scores[i], copies[i]);
+    int limit = i + scores[i];
+    if (limit > c) {
+      limit = c;
+    }
+    for (int j = i + 1; j <= limit; j++) {
+      copies[j] += copies[i];
+    }
+    total_copies += copies[i];
+  }
+  printf("%d\n", total_copies);
   return 0;
 }
